759e5a9116fd326d5fcc874749a0163de57c7782
Gonzalo Sibona <gonzalo.sibona@mercadolibre.com>
2022-10-20 14:59:31 -0300

    fix tests

--- go-web/internal/products/repository_mock_test.go
+++ go-web/internal/products/repository_mock_test.go
@@ -6,49 +6,34 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-type mockRepository struct{
-	getByIdUsed bool
+type mockDB struct{
+	readUsed bool
 }
 
-func (s *mockRepository) GetAll() ([]Product,error){
+func (db *mockDB) Read(data interface{}) error{
 	publicado := true
-	return []Product{{1,"lapicera","azul", 100.00, 130, "jwnovinsjnfi", &publicado, "20/10/22"},{2,"carpeta","rojo", 500.00, 90, "jklsfnvjfs", &publicado, "13/10/22"}},nil
-}
-func  (s *mockRepository) GetById(id int) (Product,error){
-	s.getByIdUsed = true
-	publicado := true
-	return Product{1,"before update","azul", 100.00, 130, "jwnovinsjnfi", &publicado, "20/10/22"},nil
-}
-func (s *mockRepository) Store(id int,nombre, color string, precio float64, stock int, codigo string, publicado *bool, fechaCreacion string) (Product,error){
-	return Product{},nil
-}
-func (s *mockRepository) LastID() (int,error){
-	return 0,nil
-}
-func (s *mockRepository) ModifyAll(Product) (Product,error){
-	return Product{},nil
-}
-func (s *mockRepository) Delete(id int) (Product,error){
-	return Product{}, nil
-}
-func (s *mockRepository) ModifyValues(id int, nombre string, precio float64) (Product,error){
-	product, err := s.GetById(1)
-	if err != nil {
-		return Product{}, err
+	products := &[]Product{
+		{1,"before update","azul", 100.00, 130, "jwnovinsjnfi", &publicado, "20/10/22"},
+		{2,"carpeta","rojo", 500.00, 90, "jklsfnvjfs", &publicado, "13/10/22"},
 	}
-	product.Nombre = nombre
-	return product, nil
+	t,_ := data.(*[]Product)
+	*t = *products
+	db.readUsed = true
+	return nil
 }
-
+func (db *mockDB) Write(data interface{}) error{
+	return nil
+}
 
 func TestModifyValues(t *testing.T) {
-	repository:= mockRepository{false}
-	service := NewService(&repository)
+	db := mockDB{}
+	repository:= NewRepository(&db)
 
 	esperado := "after update"
+	
+	resultado, err:= repository.ModifyValues(1,"after update", 300.00)
 
-	resultado, _ := service.ModifyValues(1,"after update", 300.00)
-
+	assert.Nil(t,err)
 	assert.Equal(t,esperado,resultado.Nombre,"deben ser iguales")
-	assert.True(t,repository.getByIdUsed,"debe ser utilizado")
+	assert.True(t,db.readUsed,"debe ser utilizado")
 }
--- go-web/internal/products/repository_stub_test.go
+++ go-web/internal/products/repository_stub_test.go
@@ -6,38 +6,35 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-type stubRepository struct{}
+type stubDB struct{}
 
-func (s stubRepository) GetAll() ([]Product,error){
+func (db stubDB) Read(data interface{}) error{
 	publicado := true
-	return []Product{{1,"lapicera","azul", 100.00, 130, "jwnovinsjnfi", &publicado, "20/10/22"},{2,"carpeta","rojo", 500.00, 90, "jklsfnvjfs", &publicado, "13/10/22"}},nil
+	products := &[]Product{
+		{1,"lapicera","azul", 100.00, 130, "jwnovinsjnfi", &publicado, "20/10/22"},
+		{2,"carpeta","rojo", 500.00, 90, "jklsfnvjfs", &publicado, "13/10/22"},
+	}
+	t,_ := data.(*[]Product)
+	*t = *products
+	return nil
 }
-func  (s stubRepository) GetById(id int) (Product,error){
-	return Product{},nil
+func (db stubDB) Write(data interface{}) error{
+	return nil
 }
-func (s stubRepository) Store(id int,nombre, color string, precio float64, stock int, codigo string, publicado *bool, fechaCreacion string) (Product,error){
-	return Product{},nil
-}
-func (s stubRepository) LastID() (int,error){
-	return 0,nil
-}
-func (s stubRepository) ModifyAll(Product) (Product,error){
-	return Product{},nil
-}
-func (s stubRepository) Delete(id int) (Product,error){
-	return Product{}, nil
-}
-func (s stubRepository) ModifyValues(id int, nombre string, precio float64) (Product,error){
-	return Product{}, nil
-}
+
 
 func TestGetAll(t *testing.T){
-	repository := stubRepository{}
-	service := NewService(repository)
-	
-	publicado := true
-	esperado:=[]Product{{1,"lapicera","azul", 100.00, 130, "jwnovinsjnfi", &publicado, "20/10/22"},{2,"carpeta","rojo", 500.00, 90, "jklsfnvjfs", &publicado, "13/10/22"}}
-	resultado,_ :=service.GetAll()
+	db := stubDB{}
+	repository := NewRepository(db)
 
-	assert.Equal(t,esperado,resultado , "deben ser iguales")
+	publicado:=true
+	esperado:= []Product{
+		{1,"lapicera","azul", 100.00, 130, "jwnovinsjnfi", &publicado, "20/10/22"},
+		{2,"carpeta","rojo", 500.00, 90, "jklsfnvjfs", &publicado, "13/10/22"},
+	}
+
+	respuesta,err := repository.GetAll()
+
+	assert.Nil(t,err)
+	assert.Equal(t,esperado,respuesta)
 }
