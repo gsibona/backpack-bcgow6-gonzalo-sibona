08ae1e5fc298a41dc5e51aa8b9bdb2f5066ad706
Gonzalo Sibona <gonzalo.sibona@mercadolibre.com>
2022-10-21 12:45:05 -0300

    add test coverage

--- go-web/internal/products/repository_mock_test.go
+++ go-web/internal/products/repository_mock_test.go
@@ -1,6 +1,7 @@
 package products
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -41,4 +42,50 @@ func TestModifyValues(t *testing.T) {
 
 	assert.Equal(t,esperado,resultado.Nombre,"deben ser iguales")
 	assert.True(t,db.readUsed,"debe ser utilizado")
+
+	esperadoError := fmt.Errorf("no existe el producto con id %d", 3)
+	
+	res2,err:= repository.ModifyValues(3,"after update", 300.00)
+
+	assert.Empty(t,res2)
+	assert.EqualError(t,esperadoError,err.Error())
+}
+
+func TestGetById(t *testing.T) {
+	db := mockDB{}
+	repository:= NewRepository(&db)
+	publicado := true
+	db.products = []Product{
+		{1,"before update","azul", 100.00, 130, "jwnovinsjnfi", &publicado, "20/10/22"},
+		{2,"carpeta","rojo", 500.00, 90, "jklsfnvjfs", &publicado, "13/10/22"},
+	}
+
+	esperado := fmt.Errorf("no existe el producto con id %d", 3)
+
+	respuesta,err := repository.GetById(3)
+	
+	assert.Empty(t,respuesta)
+	assert.EqualError(t,esperado ,err.Error())
+}
+
+
+func TestStore(t *testing.T) {
+	db := mockDB{}
+	repository:= NewRepository(&db)
+	publicado := true
+	db.products = []Product{
+		{1,"before update","azul", 100.00, 130, "jwnovinsjnfi", &publicado, "20/10/22"},
+		{2,"carpeta","rojo", 500.00, 90, "jklsfnvjfs", &publicado, "13/10/22"},
+	}
+
+	esperado := Product{3,"libro","naranja", 500.00, 90, "jklsfnvjfs", &publicado, "13/10/22"}
+
+	resultado1,err := repository.Store(3,"libro","naranja", 500.00, 90, "jklsfnvjfs", &publicado, "13/10/22")
+	assert.Nil(t,err)
+
+	resultado2,err:= repository.GetById(3)
+	assert.Nil(t,err)
+
+	assert.Equal(t,esperado,resultado1)
+	assert.Equal(t,esperado,resultado2)
 }
--- go-web/internal/products/service_test.go
+++ go-web/internal/products/service_test.go
@@ -1,10 +1,46 @@
 package products
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+func TestGetAllService(t *testing.T) {
+	publicado := true
+	db := mockDB{
+		products: []Product{{1,"before update","azul", 100.00, 130, "jwnovinsjnfi", &publicado, "20/10/22"}},
+	}
+	repository := NewRepository(&db)
+	service := NewService(repository)
+
+	esperado := []Product{{1,"before update","azul", 100.00, 130, "jwnovinsjnfi", &publicado, "20/10/22"}}
+
+	res,err := service.GetAll()
+	assert.Nil(t,err)
+	assert.Equal(t,esperado,res)
+}
+
+func TestGetByIdService(t *testing.T) {
+	publicado := true
+	db := mockDB{
+		products: []Product{{1,"before update","azul", 100.00, 130, "jwnovinsjnfi", &publicado, "20/10/22"}},
+	}
+	repository := NewRepository(&db)
+	service := NewService(repository)
+
+	esperado1:=Product{1,"before update","azul", 100.00, 130, "jwnovinsjnfi", &publicado, "20/10/22"}
+	esperado2:= fmt.Errorf("no existe el producto con id %d", 2)
+
+	res,err:= service.GetById(1)
+	assert.Nil(t,err)
+	assert.Equal(t,res,esperado1)
+
+	res,err = service.GetById(2)
+	assert.Empty(t,res)
+	assert.EqualError(t,esperado2,err.Error())
+}
 
 func TestModifyAll(t *testing.T) {
 	publicado:=true
@@ -41,4 +77,40 @@ func TestDelete(t *testing.T) {
 
 	assert.EqualError(t,err,esperado2)
 	assert.Equal(t,esperado1,res1)
+}
+
+func TestStoreService(t *testing.T) {
+	publicado:=true
+	db := mockDB{
+		products: []Product{{1,"before update","azul", 100.00, 130, "jwnovinsjnfi", &publicado, "20/10/22"}},
+	}
+	repository := NewRepository(&db)
+	service := NewService(repository)
+
+	esperado := Product{2,"new product","azul", 100.00, 130, "jwnovinsjnfi", &publicado, "20/10/22"}
+
+	res,err:= service.Store("new product","azul", 100.00, 130, "jwnovinsjnfi", &publicado, "20/10/22")
+	assert.Nil(t,err)
+	assert.Equal(t,esperado,res)
+	res,err = service.GetById(2)
+	assert.Nil(t,err)
+	assert.Equal(t,esperado,res)
+}
+
+func TestModifyValuesService(t *testing.T) {
+	publicado:=true
+	db := mockDB{
+		products: []Product{{1,"before update","azul", 100.00, 130, "jwnovinsjnfi", &publicado, "20/10/22"}},
+	}
+	repository := NewRepository(&db)
+	service := NewService(repository)
+
+	esperado := Product{1,"after update","azul", 120.00, 130, "jwnovinsjnfi", &publicado, "20/10/22"}
+
+	res,err:= service.ModifyValues(1,"after update", 120.00)
+	assert.Nil(t,err)
+	assert.Equal(t,esperado,res)
+	res,err = service.GetById(1)
+	assert.Nil(t,err)
+	assert.Equal(t,esperado,res)
 }
